ffd66f12be46bfb8ecc2772ba93f1b70dc5cde73
sefaertnc <sefaertnc@gmail.com>
2025-04-27 15:39:07 +0200

    feat(func): add function
    Add ft_memcpy from Part 1

---
+++ ft_memcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sefaertnc <sefaertnc@student.42vienna.com> :+:  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/18 09:18:36 by sefaertnc         #+#    #+#             */
+/*   Updated: 2025/04/18 09:18:36 by sefaertnc        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+	size_t				i;
+
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		d[i] = s[i];
+		++i;
+	}
+	return (dest);
+}
