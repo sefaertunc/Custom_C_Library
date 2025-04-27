275fed80285333459174068c88500b8bf2c9592d
sefaertnc <sefaertnc@gmail.com>
2025-04-27 15:46:05 +0200

    feat(func): add function
    Add ft_atoi from Part 1

---
+++ ft_atoi.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sefaertnc <sefaertnc@student.42vienna.com> :+:  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/18 09:23:05 by sefaertnc         #+#    #+#             */
+/*   Updated: 2025/04/23 11:41:47 by mertunc          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	sign;
+	int	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		++i;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		++i;
+		if (str[i] == '-' || str[i] == '+')
+			return (0);
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		++i;
+	}
+	return (result * sign);
+}
