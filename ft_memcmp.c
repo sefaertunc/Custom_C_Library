dfbcdb082b0d5ba8c1c799aa1c75c08615dd8494
sefaertnc <sefaertnc@gmail.com>
2025-04-27 15:44:55 +0200

    feat(func): add function
    Add ft_memcmp from Part 1

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sefaertnc <sefaertnc@student.42vienna.com> :+:  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/18 09:22:17 by sefaertnc         #+#    #+#             */
+/*   Updated: 2025/04/18 09:22:17 by sefaertnc        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*ptr1;
+	const unsigned char	*ptr2;
+	size_t				i;
+
+	ptr1 = (const unsigned char *)s1;
+	ptr2 = (const unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (ptr1[i] != ptr2[i])
+			return (ptr1[i] - ptr2[i]);
+		i++;
+	}
+	return (0);
+}
