bd7b5041c46b351b0cf797948efe24baa85c8577
Sefa Ertunç <sefaertnc@gmail.com>
2025-04-28 22:35:42 +0200

    feat(bonus): add function
    Add funcs from bonus part

---
+++ ft_lstclear.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*temp;
+
+	if (!lst || !del)
+		return ;
+	while (*lst != NULL)
+	{
+		temp = (*lst)->next;
+		del((*lst)->content);
+		free(*lst);
+		*lst = temp; // <-- fix here
+	}
+	*lst = NULL;
+}
