f10bfbcc69e942eb6f80f35add22d7d32b892811
sefaertnc <sefaertnc@gmail.com>
2025-04-28 15:44:08 +0200

    feat(bonus): add function
    Add ft_lstadd_front from bonus part

---
+++ ft_lstadd_front.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sefaertnc <sefaertnc@student.42vienna.com> :+:  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/28 15:27:36 by sefaertnc         #+#    #+#             */
+/*   Updated: 2025/04/28 15:27:36 by sefaertnc        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (!lst || !new)
+		return ;
+	new->next = *lst;
+	*lst = new;
+}
