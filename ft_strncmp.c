af053831b4ef2dfd54691164241b096fa9b11dde
sefaertunc <sefaertnc@gmail.com>
2025-05-09 18:11:40 +0200

    refactor(substr): Fix excessive spaces

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -19,8 +19,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		if ((unsigned char)s1[i] != (unsigned char)s2[i] ||
-			s1[i] == '\0' || s2[i] == '\0')
+		if ((unsigned char)s1[i] != (unsigned char)s2[i]
+		|| s1[i] == '\0' || s2[i] == '\0')
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		++i;
 	}
